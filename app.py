6642f03ff7059e3861bef8716edb0654fe681b0c
Omar Younis <oyounis19@gmail.com>
2024-10-09 16:30:23 +0300

    streamlit trial

---
+++ app.py
@@ -0,0 +1,13 @@
+# a simple streamlit ui for the model
+
+import streamlit as st
+import pandas as pd
+
+# add a title
+st.title('Simple Streamlit App')
+
+# add a header
+st.header('This is a header')
+
+# add a subheader
+st.subheader('This is a subheader')
