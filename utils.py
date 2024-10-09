445d3f15a3535c10526c73c8d84d26da47f1631b
Omar Younis <oyounis19@gmail.com>
2024-10-09 19:28:03 +0300

    streamlit on colab

--- utils.py
+++ utils.py
@@ -1,32 +1,29 @@
-# from groq import Groq
-
-# client = Groq(
-#     api_key="gsk_*",
-# )
-from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, Pipeline
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, pipeline
 from peft import PeftModel
 
-bnb_config = BitsAndBytesConfig(
-    load_in_8bit=True,
-    llm_int8_threshold=5.0, # Lower to save more memory, by converting more params to int8
-    llm_int8_skip_modules=None # quantize all modules
-)
+def setup_pipeline():
+    bnb_config = BitsAndBytesConfig(
+        load_in_8bit=True,
+        llm_int8_threshold=5.0,  # Lower to save more memory, by converting more params to int8
+        llm_int8_skip_modules=None  # quantize all modules
+    )
 
-# Load the llm and tokenizer
-tokenizer = AutoTokenizer.from_pretrained("microsoft/Phi-3.5-mini-instruct", trust_remote_code=True)
-base_model = AutoModelForCausalLM.from_pretrained("microsoft/Phi-3.5-mini-instruct",
-                                                device_map="auto", 
-                                                quantization_config=bnb_config, 
-                                                trust_remote_code=True
-)
+    # Load the llm and tokenizer
+    tokenizer = AutoTokenizer.from_pretrained("microsoft/Phi-3.5-mini-instruct", trust_remote_code=True)
+    base_model = AutoModelForCausalLM.from_pretrained("microsoft/Phi-3.5-mini-instruct",
+                                                      device_map="auto", 
+                                                      quantization_config=bnb_config, 
+                                                      trust_remote_code=True)
 
-# Combine the model with the LORA adapter
-model_with_lora = PeftModel.from_pretrained(base_model, "oyounis/Phi-3.5-instruct-pubmedQA")
+    # Combine the model with the LORA adapter
+    model_with_lora = PeftModel.from_pretrained(base_model, "oyounis/Phi-3.5-instruct-pubmedQA")
 
-pipe = Pipeline("text-generation",
-                model=model_with_lora,
-                tokenizer=tokenizer
-)
+    # Create the pipeline
+    pipe = pipeline("text-generation",
+                    model=model_with_lora,
+                    tokenizer=tokenizer)
+
+    return pipe
 
 sys_prompt = """You are a professional medical assistant trained to provide accurate and reliable information to patients. 
 Instructions:
